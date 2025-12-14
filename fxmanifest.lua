7e49083a30420184d7886f491f31422af3f92f81
Matti <138713607+mattibat@users.noreply.github.com>
2025-12-14 17:11:50 +0100

    Initialize fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_script 'client.lua'
+
+files {
+    'html/img/*.png',
+    'html/index.html'
+}
